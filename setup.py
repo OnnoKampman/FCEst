fb43bf2a7efc8db3e6a55541409dcb09d3822e87
Onno Kampman <onno.kampman@gmail.com>
2023-06-08 17:27:43 +0100

    clean up setup script

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
 
 setup(
-    name='FCEst',
+    name='fcest',
     version='0.0.1',
     description='Methods for estimation of functional connectivity',
     url='http://github.com/OnnoKampman/FCEst',
     author='Onno P. Kampman',
     author_email='onno.kampman@gmail.com',
-    license='Apache 2.0',
-    packages=['FCEst'],
+    license='Apache License 2.0',
+    packages=['fcest'],
     install_requires=[
         'gpflow==2.5.2',
-        'matplotlib',
         'numpy',
         'pandas',
-        'scikit-learn',
+        'scipy',
         'tensorflow',
     ],
+    python_requires='>=3.8',
     zip_safe=False
 )
