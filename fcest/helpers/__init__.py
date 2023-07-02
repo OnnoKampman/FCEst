1d52a2bdd9e56a04805a229f7ab8d094036edb5b
Onno Kampman <onno.kampman@gmail.com>
2023-07-02 14:00:13 +0100

    add WP inference helper scripts

--- fcest/helpers/__init__.py
+++ fcest/helpers/__init__.py
@@ -1,6 +1,7 @@
 from .array_operations import are_all_positive_definite, convert_tensor_to_correlation, to_correlation_structure
 from .data import to_3d_format
 from .filtering import highpass_filter_data
+from .inference import run_adam_vwp, run_adam_svwp
 
 __all__ = [
     "to_correlation_structure",
@@ -8,4 +9,6 @@ __all__ = [
     "are_all_positive_definite",
     "to_3d_format",
     "highpass_filter_data",
+    "run_adam_vwp",
+    "run_adam_svwp",
 ]
