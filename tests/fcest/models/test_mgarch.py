2d1a729ad4355e192bc03db6b4efe135f35ec573
Onno Kampman <onno.kampman@gmail.com>
2023-07-11 13:31:27 +0100

    add unit test for MGARCH model

--- tests/fcest/models/test_mgarch.py
+++ tests/fcest/models/test_mgarch.py
@@ -3,6 +3,7 @@ import unittest
 
 import numpy as np
 from numpy.testing import assert_array_equal
+import pandas as pd
 
 from fcest.models.mgarch import MGARCH
 
@@ -16,8 +17,33 @@ logging.basicConfig(
 class TestMGARCH(unittest.TestCase):
     """Test the MGARCH class."""
 
-    def test_static_covariance_estimate_bivariate(self):
-        assert True == True
+    def test_mgarch(self):
+        """Test various instantiations of the MGARCH class."""
+
+        # In the bivariate case (D = 2), joint and pairwise estimation should be equivalent.
+        m = MGARCH(
+            mgarch_type='DCC'
+        )
+        m.fit_model(
+            training_data_df=self._get_dummy_training_data(),
+            training_type='joint'
+        )
+        cov_structure_joint = m.train_location_covariance_structure
+        m.fit_model(
+            training_data_df=self._get_dummy_training_data(),
+            training_type='pairwise'
+        )
+        cov_structure_pairwise = m.train_location_covariance_structure
+        assert_array_equal(cov_structure_joint, cov_structure_pairwise)
+
+    @staticmethod
+    def _get_dummy_training_data(
+        n_time_series: int = 2, n_time_steps: int = 400
+    ) -> pd.DataFrame:
+        np.random.seed(2023)
+        return pd.DataFrame(
+            np.random.normal(size=(n_time_steps, n_time_series))
+        )
 
 
 if __name__ == "__main__":
