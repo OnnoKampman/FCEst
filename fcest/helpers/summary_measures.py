d89874dcbf9d6e9b5f10c9f436e21343d54845cb
Onno Kampman <onnokampman@Onnos-MacBook-Pro-MOHT.local>
2024-01-29 17:37:43 +0800

    add AR1 model as summary measure

--- fcest/helpers/summary_measures.py
+++ fcest/helpers/summary_measures.py
@@ -1,33 +1,64 @@
 import logging
 
 import numpy as np
+from statsmodels.tsa.ar_model import AutoReg
+
+from .array_operations import get_all_lower_triangular_indices_tuples
 
 
 def summarize_tvfc_estimates(
         full_covariance_structure: np.array, tvfc_summary_metric: str
 ) -> np.array:
     """
-    Summarize a full TVFC covariance structure.
+    Summarize a full TVFC covariance structure over temporal axis.
+
     :param full_covariance_structure: TVFC array of shape (N, D, D)
     :param tvfc_summary_metric:
     :return:
         array of shape (D, D)
     """
     match tvfc_summary_metric:
-        case 'mean':
-            return np.nanmean(full_covariance_structure, axis=0)  # (D, D)
-        case 'variance':
-            return np.nanvar(full_covariance_structure, axis=0)  # (D, D)
-        case 'std':
-            return np.nanstd(full_covariance_structure, axis=0)  # (D, D)
-        case 'rate_of_change':
-            return compute_rate_of_change(full_covariance_structure)  # (D, D)
-        case 'fourier_mean':
-            raise NotImplementedError
+        case 'ar1':
+            return fit_and_extract_ar1_param(full_covariance_structure)  # (D, D)
         case 'fourier_max':
             raise NotImplementedError
+        case 'fourier_mean':
+            raise NotImplementedError
+        case 'mean':
+            return np.nanmean(full_covariance_structure, axis=0)  # (D, D)
+        case 'rate_of_change':
+            return compute_rate_of_change(full_covariance_structure)  # (D, D)
+        case 'std':
+            return np.nanstd(full_covariance_structure, axis=0)  # (D, D)
+        case 'variance':
+            return np.nanvar(full_covariance_structure, axis=0)  # (D, D)
         case _:
             logging.error(f"TVFC summary metric {tvfc_summary_metric:s} not recognized.")
+
+
+def fit_and_extract_ar1_param(full_covariance_structure: np.array) -> np.array:
+    """
+    Summarize estimated TVFC by taking AR(1) component of each time series.
+    Diagonal terms are set to zero at the moment.
+    """
+    indices = get_all_lower_triangular_indices_tuples(n_time_series=full_covariance_structure.shape[1])
+
+    ar1_coefficients = np.zeros(
+        shape=(full_covariance_structure.shape[1], full_covariance_structure.shape[2])
+    )
+    for tup_i, tup_j in indices:
+        model = AutoReg(
+            full_covariance_structure[:, tup_i, tup_j],
+            lags=1
+        )
+        model_fit = model.fit()
+
+        # print(model_fit.summary())
+        # print('Coefficients: %s' % model_fit.params)
+
+        ar1_coefficients[tup_i, tup_j] = ar1_coefficients[tup_j, tup_i] = model_fit.params[-1]
+
+    return ar1_coefficients
 
 
 def compute_rate_of_change(full_covariance_structure: np.array) -> np.array:
